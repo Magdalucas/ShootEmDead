66dbb2b9d6200b3b2cb3014755d437a848e1bcb8
Magdalucas <magdalenalucas2004@gmail.com>
2023-05-01 17:47:31 -0300

    Shootemdead1

---
+++ Shootemdead/Game/Bullet2.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    public class Bullet2
+    {
+        /* Character Properties */
+        private Transform _transform;
+        private Renderer _renderer;
+        private Animation idleAnimation;
+        private Animation currentAnimation;
+        private Transform _playerTransform;
+        private Character _player2;
+
+        /* Speed Values */
+        private float _movementSpeed;
+        private float _rotationSpeed;
+
+        #region PUBLIC_METODS
+
+        public Bullet2(Vector2 position, Vector2 scale, float angle, float movementSpeed)
+        {
+            _player2 = LevelController.Player;
+            _player2.LifeController.onGetDamage += OnGetDamageHandler;
+
+            _transform = new Transform(position, scale, angle);
+
+
+            CreateAnimations();
+            currentAnimation = idleAnimation;
+            _movementSpeed = movementSpeed;
+            _rotationSpeed = 100f;
+
+            _renderer = new Renderer(idleAnimation, scale);
+
+
+
+        }
+
+        private void OnGetDamageHandler()
+        {
+            Engine.Debug("acá podría hacer desaparecer la bala");
+        }
+
+        private void CreateAnimations()
+        {
+            List<Texture> idleTextures = new List<Texture>();
+            for (int i = 0; i < 4; i++)
+            {
+                Texture frame = Engine.GetTexture($"Textures/Bullet2/Idle/{i}.png");
+                idleTextures.Add(frame);
+            }
+            idleAnimation = new Animation("Idle", idleTextures, 0.1f, true);
+
+        }
+
+
+        public void Initialize() { }
+
+        public void Update()
+        {
+
+            _transform.Translate(new Vector2(1, 0), _movementSpeed);
+            //    _transform.Rotate(1, _rotationSpeed);
+
+            if (_transform.Position.X >= 1280 + _renderer.Texture.Width)
+                _transform.SetPositon(new Vector2(-_renderer.Texture.Width, _transform.Position.Y));
+
+            currentAnimation.Update();
+            CheckCollision();
+        }
+
+        public void CheckCollision()
+        {
+
+            float distanceX = Math.Abs(_player2.Transform.Position.X - _transform.Position.X);
+            float distanceY = Math.Abs(_player2.Transform.Position.Y - _transform.Position.Y);
+
+            float sumHalfWidths = _player2.Renderer.Texture.Width / 2 + _renderer.Texture.Width / 2;
+            float sumHalfHeights = _player2.Renderer.Texture.Height / 2 + _renderer.Texture.Height / 2;
+
+            if (distanceX <= sumHalfWidths && distanceY <= sumHalfHeights)
+            {
+                _player2.LifeController.GetDamage(50);
+            }
+        }
+
+        public void Render()
+        {
+            _renderer.Render(_transform);
+        }
+
+        #endregion
+
+
+    }
+}
---
+++ Shootemdead/Game/LevelController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    public class LevelController
+    {
+        private static List<Enemy> enemies = new List<Enemy>();
+        private static List<Character> player = new List<Character>();
+        private static List<Bullet> bullets = new List<Bullet>();
+
+        private static Character _player;
+        private static Character2 _player2;
+
+        public static Character Player => _player;
+        public static Character2 Player2 => _player2;
+
+        /// Timeframe Calculation Properties
+        private  Time _time;
+
+
+        public void Initialization()
+    {
+        _time.Initialize();
+            _player = new Character("Textures/Soldierizq/Idle1.png", new Vector2(100, 200), new Vector2(1.50f, 1.50f), 0, 100);
+            _player2 = new Character2("Textures/Soldierder/Idle1.png", new Vector2(900, 200), new Vector2(1.50f, 1.50f), 0, 100);
+
+            //enemies.Add(new Enemy("Textures/ship.png", new Vector2(0, 400), new Vector2(.75f, .75f), 0, 100));
+            //enemies.Add(new Enemy("Textures/ship.png", new Vector2(0, 600), new Vector2(.75f, .75f), 0, 100));
+        }
+
+    public void Update()
+    {
+        _time.Update();
+        _player.Update();
+         _player2.Update();
+        foreach (Enemy enemy in enemies) enemy.Update();
+        foreach (Bullet bullet in bullets) bullet.Update();
+        }
+
+        public static void CreateBullet(Vector2 position)
+        {
+            bullets.Add(new Bullet(new Vector2(Player.Transform.Position.X + 50, Player.Transform.Position.Y), new Vector2(1f, 1f), 0, 80));
+        }
+
+        public static void CreateBullet2(Vector2 position)
+        {
+            bullets.Add (new Bullet(new Vector2(Player2.Transform.Position.X - 50, Player2.Transform.Position.Y), new Vector2(-1f, -1f), 0, -80));
+        }
+
+
+        public void Render()
+    {
+        Engine.Clear();
+
+        _player.Render();
+            _player2.Render();
+            foreach (Enemy enemy in enemies) enemy.Render();
+        foreach (Bullet bullet in bullets) bullet.Render();
+
+        Engine.Show();
+    }
+}
+}
